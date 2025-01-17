b038ca6751d1a85e4e7f63a1e66fe924b13f867e
Marckenley Dorsainvil <marckenlygbeau@gmail.com>
2025-01-17 11:41:00 +0400

    adding create new hotel feature

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,7 @@ function Header() {
             <span className="flex space-x-2">
               {isLoggedIn ? <>
                 <Link className="flex items-center text-white px-3 font-semibold hover:bg-blue-800" to="/my-booking">My Bookings</Link>
-                <Link className="flex items-center text-white px-3 font-semibold hover:bg-blue-800" to="my-hotels">My Hotels</Link>
+                <Link className="flex items-center text-white px-3 font-semibold hover:bg-blue-800" to="/my-hotels">My Hotels</Link>
                 <SignOutButton/>
                 </> :
                  <> <Link className="text-blue-800 flex items-center px-3 font-semibold hover:bg-gray-100 bg-white cursor-pointer" to="/signin">Sign In</Link> </> }

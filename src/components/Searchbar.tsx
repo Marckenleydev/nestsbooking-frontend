2d0301ad98f457b52713c997775710275bc1e7d4
Marckenley Dorsainvil <marckenlygbeau@gmail.com>
2025-03-06 15:32:22 +0400

    modified the place holder

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -37,7 +37,7 @@ const Searchbar = () => {
       <div className="flex flex-row items-center flex-1 bg-white p-2">
         <MdTravelExplore size={25} className="mr-2" />
         <input
-          placeholder="Where are you going?"
+          placeholder="Where are you going, dubai, london ?"
           className="text-md w-full focus:outline-none"
           value={destination}
           onChange={(event) => setDestination(event.target.value)}
